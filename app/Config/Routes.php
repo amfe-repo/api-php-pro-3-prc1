909f7e697c415dc2175b1c636e4065cdb80a1e53
Yisus1119 <jesusemigenio@gmail.com>
2022-03-19 00:27:04 -0400

    subiendo cambios

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -40,26 +40,30 @@ $routes->get('/', 'Home::index');
 $routes->get('api/entities','API\Entities::index');
 $routes->post('api/entities/create','API\Entities::create');
 $routes->get('/api/entities/search/(:num)', 'API\Entities::search/$1');
+$routes->put('/api/entities/update/(:num)', 'API\Entities::update/$1');
+$routes->delete('/api/entities/delete/(:num)','API\Entities::delete/$1');
 
 // Categories routes http://localhost:8080/api/categories
 $routes->get('api/categories','API\Categories::index');
 $routes->post('api/categories/create','API\Categories::create');
-$routes->get('api/categories/search/(:num)', 'API\Categories::search/$1');
+$routes->get('/api/categories/search/(:num)', 'API\Categories::search/$1');
+$routes->put('/api/categories/update/(:num)','API\Categores::update/$1');
+$routes->delete('/api/categories/delete/(:num)','API\Categories::delete/$1');
 
 //JobsPosted routes http://localhost:8080/api/jobsposted
 $routes->get('api/jobsposted','API\JobsPosted::index');
 $routes->post('api/jobsposted','API\Jobsposted::create');
-$routes->get('api/jobsposted/search/(:num)', 'API\Jobsposted::search/$1');
+$routes->get('/api/jobsposted/search/(:num)', 'API\Jobsposted::search/$1');
 
 //ScheduleJob  routes http://localhost:8080/api/schedulejob
 $routes->get('api/schedulejob','API\ScheduleJob::index');
 $routes->post('api/schedulejob','API\ScheduleJob::create');
-$routes->get('api/schedulejob/search/(:num)', 'API\ScheduleJob::search/$1');
+$routes->get('/api/schedulejob/search/(:num)', 'API\ScheduleJob::search/$1');
 
 //Bussiness routes http://localhost:8080/api/bussiness
 $routes->get('api/bussiness','API\Bussiness::index');
 $routes->post('api/bussiness','API\Bussiness::create');
-$routes->get('api/bussiness/search/(:num)', 'API\Bussiness::search/$1');
+$routes->get('/api/bussiness/search/(:num)', 'API\Bussiness::search/$1');
 
 
 /*
